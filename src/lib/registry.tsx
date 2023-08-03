eae673ba88eb9d8472269847623a1e19838e8bad
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-08-03 15:27:22 +0200

    refactor type for registry

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import React, { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
-const StyledComponentsRegistry = ({
-  children,
-}: {
-  children: React.ReactNode
-}) => {
+type Props = {
+  children: ReactNode
+}
+
+const StyledComponentsRegistry = ({ children }: Props) => {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
   useServerInsertedHTML(() => {
