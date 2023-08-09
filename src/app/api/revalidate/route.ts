7db30fef2578d66c1e6e17cfc5fe118017fb58f1
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-08-09 16:07:00 +0200

    refactor revalidateTag function

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag } from 'next/cache'
 
-export async function GET(request: NextRequest) {
+export const GET = async (request: NextRequest) => {
   const tag = request.nextUrl.searchParams.get('tag')!
   revalidateTag(tag)
   return NextResponse.json({ revalidated: true })
