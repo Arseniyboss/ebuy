a0c5312dd98dcde0bdc8085ec9762860d549a3be
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-12-05 20:15:54 +0100

    use setCookie function in logout route handler

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,11 +1,8 @@
+import { setCookie } from '@/utils/api/setCookie'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
   const response = NextResponse.next()
   const tokenCookie = response.cookies.delete('token')
-
-  return NextResponse.json(null, {
-    status: 200,
-    headers: { 'Set-Cookie': tokenCookie.toString() },
-  })
+  return setCookie(tokenCookie.toString())
 }
