e9284113a359df43bd0ab5095669224d649c6e38
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2024-01-19 11:51:53 +0100

    change BASE_URL env variable

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -1,3 +1,2 @@
-const LOCAL_URL = 'http://localhost:3000'
-const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-export const BASE_URL = VERCEL_URL ? `https://${VERCEL_URL}` : LOCAL_URL
+const LOCAL_BASE_URL = 'http://localhost:3000'
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || LOCAL_BASE_URL
