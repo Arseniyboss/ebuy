b351d40beef7ff90105490c76d1a6ba62501492d
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2024-01-19 11:19:00 +0100

    change BASE_URL env variable

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000'
+const LOCAL_BASE_URL = 'http://localhost:3000'
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || LOCAL_BASE_URL
