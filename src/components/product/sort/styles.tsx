0e6fe7446d177436ec0d63cf51f42c2c42ff3aa5
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2024-01-20 08:53:18 +0100

    set default text alignment in sort select

--- src/components/product/sort/styles.tsx
+++ src/components/product/sort/styles.tsx
@@ -5,6 +5,7 @@ import { breakpoints } from '@/constants/breakpoints'
 export const SortSelect = styled(Select)`
   @media screen and (max-width: ${breakpoints.home.small}) {
     text-align: start;
+    text-align-last: start;
     width: 100%;
     border-radius: 0.3rem;
     padding: 0.7rem 1rem;
