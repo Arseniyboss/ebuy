0e3beea170f5256d3a9b43bd87628043588b8427
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-09-02 08:34:42 +0200

    add error handling to stripe webhook api

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { stripe } from '@/utils/api/stripe'
 import { updateOrderToPaid } from '@/api/orders/updateOrderToPaid'
 import { revalidateTag } from '@/api/revalidateTag'
+import { throwError } from '@/utils/api/throwError'
 
 type Metadata = {
   orderId: string
@@ -13,18 +14,24 @@ export const POST = async (request: NextRequest) => {
   const signature = request.headers.get('stripe-signature')!
   const secret = process.env.STRIPE_WEBHOOK_SECRET
 
-  const event = stripe.webhooks.constructEvent(body, signature, secret)
+  try {
+    const event = stripe.webhooks.constructEvent(body, signature, secret)
 
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object as Stripe.Checkout.Session
-    const metadata = session.metadata as Metadata
-    const orderId = metadata.orderId
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object as Stripe.Checkout.Session
+      const metadata = session.metadata as Metadata
+      const orderId = metadata.orderId
 
-    const { error } = await updateOrderToPaid(orderId, session.id)
-    if (error) return alert(error)
+      const { error } = await updateOrderToPaid(orderId, session.id)
+      if (error) return alert(error)
 
-    await revalidateTag('order')
+      await revalidateTag('order')
+    }
+
+    return NextResponse.json(null)
+  } catch (error) {
+    if (error instanceof Error) {
+      return throwError({ error: error.message, status: 400 })
+    }
   }
-
-  return NextResponse.json(null)
 }
