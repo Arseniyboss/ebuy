a36cffa6aad7a91ace6510b1d89b6b67d3337c58
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-09-05 15:03:54 +0200

    add error handling to fetchData api

--- src/utils/api/fetchData.ts
+++ src/utils/api/fetchData.ts
@@ -30,23 +30,27 @@ export const fetchData = async <T>(
 ): Promise<Response<T>> => {
   const accessToken = await getAccessToken()
 
-  const response = await fetch(`${BASE_URL}/api${route}`, {
-    method: options?.method || 'GET',
-    body: JSON.stringify(options?.body),
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${accessToken || options?.stripeSessionId}`,
-    },
-    next: {
-      tags: options?.tags,
-    },
-  })
+  try {
+    const response = await fetch(`${BASE_URL}/api${route}`, {
+      method: options?.method || 'GET',
+      body: JSON.stringify(options?.body),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken || options?.stripeSessionId}`,
+      },
+      next: {
+        tags: options?.tags,
+      },
+    })
 
-  if (!response.ok) {
-    const { error }: Error = await response.json()
-    return { error }
+    if (!response.ok) {
+      const { error }: Error = await response.json()
+      return { error }
+    }
+
+    const data: T = await response.json()
+    return { data }
+  } catch (error) {
+    return { error: 'Fetch Failed' }
   }
-
-  const data: T = await response.json()
-  return { data }
 }
