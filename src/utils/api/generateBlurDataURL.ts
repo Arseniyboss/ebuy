48b36721ab0515ed567a57622791b4dcb281a92c
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-11-09 15:06:34 +0100

    fix product image path for Vercel

--- src/utils/api/generateBlurDataURL.ts
+++ src/utils/api/generateBlurDataURL.ts
@@ -1,7 +1,9 @@
+import path from 'path'
 import sharp from 'sharp'
 
 export const generateBlurDataURL = async (src: string) => {
-  const imageBuffer = await sharp(`public/${src}`).toBuffer()
+  const imagePath = path.join(process.cwd(), 'public', src.replace(/^\/+/, ''))
+  const imageBuffer = await sharp(imagePath).toBuffer()
   const base64Image = imageBuffer.toString('base64')
   const blurDataURL = `data:image/jpg;base64,${base64Image}`
   return blurDataURL
