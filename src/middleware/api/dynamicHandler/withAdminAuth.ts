ca905b9f3aa69a263953fe59956b3de9ed1324a0
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-11-13 18:59:06 +0100

    use withAuth in withAdminAuth

--- src/middleware/api/dynamicHandler/withAdminAuth.ts
+++ src/middleware/api/dynamicHandler/withAdminAuth.ts
@@ -1,18 +1,12 @@
-import { NextRequest } from 'next/server'
 import { Handler } from './types'
-import { PageParams } from '@/types/params'
+import { withAuth } from './withAuth'
 import { throwError } from '@/utils/api/throwError'
-import { getUser } from '@/utils/api/getUser'
 
 export const withAdminAuth = <T>(handler: Handler<T>) => {
-  return async (request: NextRequest, { params }: PageParams) => {
-    const user = await getUser(request)
-    if (!user) {
-      return throwError({ error: 'User not found', status: 404 })
-    }
+  return withAuth(async ({ request, user, params }) => {
     if (!user.isAdmin) {
       return throwError({ error: 'Not authorized', status: 401 })
     }
     return handler({ request, user, params })
-  }
+  })
 }
